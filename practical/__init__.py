5763ffd4e049dec594bd24292dbd6e576fedda79
apehex <apehex@protonmail.com>
2018-10-02 22:27:01 +0200

    Bump version: 0.0.0 → 0.0.1

--- practical/__init__.py
+++ practical/__init__.py
@@ -36,7 +36,7 @@ from practical.web import (
 
 __author__ = """apehex"""
 __email__ = 'apehex@protonmail.com'
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 __all__ = [
     'convert_dict_to_array',
